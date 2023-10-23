c105ae3457d4ffdafca9480fab79de3adfb9f53e
Tri Hoang <79836132+trihoang0809@users.noreply.github.com>
2023-10-23 17:45:06 -0500

    Update recommendation.py

--- recommendation.py
+++ recommendation.py
@@ -1,17 +1,23 @@
 import requests
 
-FPL_API_URL = "https://api.fpl.com/data"
+FPL_API_URL = "https://fantasy.premierleague.com/api/bootstrap-static/"
 
-def fetch_current_form():
-    response = requests.get(f"{FPL_API_URL}/players-form")
-    return response.json()
+def fetch_players_data():
+    response = requests.get(FPL_API_URL)
+    data = response.json()
+    return data['elements']
 
-def recommend_players_based_on_form(players_form):
-    # A simplified recommendation logic based on players' form
-    top_players = sorted(players_form, key=lambda x: x['form'], reverse=True)[:10]
+def recommend_players_based_on_form(players_data):
+    # A simple recommendation logic based on players' form and points
+    top_players = sorted(players_data, key=lambda x: (float(x['form']), x['total_points']), reverse=True)[:10]
     return top_players
 
+def display_recommendations(recommendations):
+    print("\nTop recommended players:")
+    for player in recommendations:
+        print(f"Name: {player['first_name']} {player['second_name']}, Team: {player['team']}, Form: {player['form']}, Total Points: {player['total_points']}")
+
 if __name__ == "__main__":
-    players_form = fetch_current_form()
-    top_recommendations = recommend_players_based_on_form(players_form)
-    print("Top recommended players:", top_recommendations)
+    players_data = fetch_players_data()
+    recommendations = recommend_players_based_on_form(players_data)
+    display_recommendations(recommendations)
